2830e871e7406f478900251add9d7094630754cd
Garx1212 <113130182+Garx1212@users.noreply.github.com>
2024-10-01 02:53:38 +0000

    generador de cartas

--- src/app.js
+++ src/app.js
@@ -4,8 +4,49 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const theCardId = document.getElementById("cardId");
+  let icons = ["♦", "♥", "♠", "♣"];
+  let character = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  let colorClass = "text-black";
+
+  let randomIcon = Math.floor(Math.random() * icons.length);
+  let currentIcon = icons[randomIcon];
+
+  if (currentIcon === "♥" || currentIcon === "♦") {
+    colorClass = "text-danger";
+  }
+
+  let randomNum = Math.floor(Math.random() * character.length);
+  let currentCharacter = character[randomNum];
+
+  let thisHTML = `<div class="d-flex justify-content-start mb-5 p-2 ${colorClass}">
+  <span class="fs-1">${currentIcon}</span>
+  </div>
+
+  <div>
+    <h1 class="d-flex justify-content-center mt-5 mb-5 p-5 ${colorClass}">${currentCharacter}</h1>
+  </div>
+
+  <div class="d-flex justify-content-end mt-5 p-2 ${colorClass}">
+  <span class="fs-1" style="transform:rotate(180deg)">${currentIcon}</span>
+  </div>`;
+
+  theCardId.innerHTML = thisHTML;
+  console.log(Math.floor(Math.random() * 13 + 1));
 };
